8265bb9d68d2431832c619f2eecaccc375164442
strider38 <80421367+strider38@users.noreply.github.com>
2021-06-07 15:06:25 +0300

    Update my bot.py

--- my bot.py
+++ my bot.py
@@ -1,10 +1,6 @@
 import discord
 from discord import utils
 
-
-import config
-
-
 class MyClient(discord.Client):
     intents = discord.Intents.all()
     client = discord.Client(intents=intents)
@@ -13,16 +9,16 @@ class MyClient(discord.Client):
         print('Logged on as {0}!'.format(self.user))
 
     async def on_raw_reaction_add(self, payload):
-        if payload.message_id == config.POST_ID:
+        if payload.message_id == POST_ID:
             channel = self.get_channel(payload.channel_id)  # получаем объект канала
             message = await channel.fetch_message(payload.message_id)  # получаем объект сообщения
             member = payload.member  # получаем объект пользователя который поставил реакцию
 
             try:
                 emoji = str(payload.emoji)  # эмоджик который выбрал юзер
-                role = utils.get(message.guild.roles, id=config.ROLES[emoji])  # объект выбранной роли (если есть)
+                role = utils.get(message.guild.roles, id=ROLES[emoji])  # объект выбранной роли (если есть)
 
-                if len([i for i in member.roles if i.id not in config.EXCROLES]) <= config.MAX_ROLES_PER_USER:
+                if len([i for i in member.roles if i.id not in EXCROLES]) <= MAX_ROLES_PER_USER:
                     await member.add_roles(role)
                     print('[SUCCESS] User {0.display_name} has been granted with role {1.name}'.format(member, role))
                 else:
@@ -42,7 +38,7 @@ class MyClient(discord.Client):
 
         try:
             emoji = str(payload.emoji)  # эмоджик который выбрал юзер
-            role = utils.get(message.guild.roles, id=config.ROLES[emoji])  # объект выбранной роли (если есть)
+            role = utils.get(message.guild.roles, id=ROLES[emoji])  # объект выбранной роли (если есть)
 
             await member.remove_roles(role)
             print('[SUCCESS] Role {1.name} has been remove for user {0.display_name}'.format(member, role))
@@ -55,4 +51,4 @@ class MyClient(discord.Client):
 
 # RUN
 client = MyClient()
-client.run(config.TOKEN)
+client.run(TOKEN)
